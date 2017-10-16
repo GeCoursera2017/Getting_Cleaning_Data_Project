c5449dec7087702550de055ac25503b9c01500ce
GeCoursera2017 <1005645460@qq.com>
2017-10-16 18:25:24 +0800

    Second Commit

--- run_analysis.R
+++ run_analysis.R
@@ -2,63 +2,45 @@ library(tidyr)
 library(reshape2)
 library(dplyr)
 library(stringr)
+## Before running this script, put "UCI HAR Dataset" in the same folder as run_analysis.R
+## Setting working directory
 this.dir <- dirname(parent.frame(2)$ofile)
-wkDir <- gsub(" ","",paste(this.dir,"/UCI_HAR_Dataset",""))
-setwd(wkDir) # set working directory
-
-## Define function to remove empty elements
-func <- function(Lchar){
-  Lchar[which(Lchar !="")]
-}
-
-## fileConnections and Data Loading
-fileConnect <- file('./test/X_test.txt')
-fileConnectTrain <- file('./train/X_train.txt')
-measurementName <- read.csv('features.txt',sep=" ",header = F,stringsAsFactors = F)
+setwd(this.dir) 
 
 # Loading Test Data
-L <- readLines(fileConnect)
-L2<-strsplit(L,split = " ")
-L3<-t(sapply(L2,func))
-testDF<-data.frame(matrix(as.numeric(L3),ncol=561))
-testSubject <- read.csv('./test/subject_test.txt',sep = " ",header = F,stringsAsFactors = F)
-testActivityID <-  read.csv('./test/y_test.txt',sep = " ",header = F,stringsAsFactors = F)
+testDF <- read.table('./UCI HAR Dataset/test/X_test.txt')
+testSubject <- read.csv('./UCI HAR Dataset/test/subject_test.txt',sep = " ",header = F,stringsAsFactors = F)
+testActivityID <-  read.csv('./UCI HAR Dataset/test/y_test.txt',sep = " ",header = F,stringsAsFactors = F)
+testDF <- cbind(testDF,testSubject,testActivityID)
 
 # Loading Train Data
-L <- readLines(fileConnectTrain)
-L2<-strsplit(L,split = " ")
-L3<-t(sapply(L2,func))
-trainDF<-data.frame(matrix(as.numeric(L3),ncol=561))
-trainSubject <- read.csv('./train/subject_train.txt',sep = " ",header = F,stringsAsFactors = F)
-trainActivityID <-  read.csv('./train/y_train.txt',sep = " ",header = F,stringsAsFactors = F)
-
-# Close file connections
-close(fileConnect)
-close(fileConnectTrain)
+trainDF <- read.table('./UCI HAR Dataset/train/X_train.txt')
+trainSubject <- read.csv('./UCI HAR Dataset/train/subject_train.txt',sep = " ",header = F,stringsAsFactors = F)
+trainActivityID <-  read.csv('./UCI HAR Dataset/train/y_train.txt',sep = " ",header = F,stringsAsFactors = F)
+trainDF <- cbind(trainDF,trainSubject,trainActivityID)
 
 
 ## Mofidy col names and merge data
-testDF <- cbind(testDF,testSubject,testActivityID)
-trainDF <- cbind(trainDF,trainSubject,trainActivityID)
-names(testDF) <- c(as.vector(measurementName$V2),"subjectID","activityID")
-names(trainDF) <- c(as.vector(measurementName$V2),"subjectID","activityID")
+measurementName <- read.csv('UCI HAR Dataset/features.txt',sep=" ",header = F,stringsAsFactors = F)
 mergedDF <- rbind(testDF,trainDF)
+names(mergedDF) <- c(as.vector(measurementName$V2),"subjectID","activityID")
+
 
 # Extract only means,  stds,  subjectIDs and activityIDs
 selectedIndex <- grep("mean|std",measurementName$V2)
 selectedDF <- mergedDF[,c(selectedIndex,562,563)]
 
 # Use decriptive activity names
-activityLables <-  read.csv('activity_labels.txt',sep=" ",header = F,stringsAsFactors = F)
+activityLables <-  read.csv('UCI HAR Dataset/activity_labels.txt',sep=" ",header = F,stringsAsFactors = F)
 selectedDF<- selectedDF %>% mutate(activityNames =activityLables$V2[activityID] ) %>% select(-activityID)
 
 # Reshape data frame to desired tidy dataset
 finalDF<- selectedDF %>% 
-    gather(measurements,value,-(subjectID:activityNames)) %>% 
-    unite(subjectID_activityNames,subjectID,activityNames,sep="_")  %>% 
-    dcast(subjectID_activityNames ~ measurements,mean) %>%
-    separate(subjectID_activityNames,into =c("subjectID","activityNames"),sep="_") %>%
-    gather(measurements,averageValue,-(subjectID:activityNames))
+  gather(measurements,value,-(subjectID:activityNames)) %>% 
+  unite(subjectID_activityNames,subjectID,activityNames,sep="_")  %>% 
+  dcast(subjectID_activityNames ~ measurements,mean) %>%
+  separate(subjectID_activityNames,into =c("subjectID","activityNames"),sep="_") %>%
+  gather(measurements,averageValue,-(subjectID:activityNames))
 
 # Write dataset to txt files
 write.table(selectedDF,file = "firstDataset.txt",row.names=F)
